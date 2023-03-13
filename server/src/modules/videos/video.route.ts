b62a2f3e438b3f3b51dca8f6470f5474f8246047
Ismael R <ismael_br7@hotmail.com>
2023-03-12 22:42:25 -0600

    [Updated] Video stream controller, get all videos

--- server/src/modules/videos/video.route.ts
+++ server/src/modules/videos/video.route.ts
@@ -1,10 +1,21 @@
 import express from 'express';
 
 import requireUser from '../../middleware/requireUser';
-import { uploadVideoHandler } from './video.controller';
+import {
+  findVideosHandler,
+  streamVideoHandler,
+  updateVideoHandler,
+  uploadVideoHandler,
+} from './video.controller';
 
 const router = express.Router();
 
 router.post('/', requireUser, uploadVideoHandler);
+
+router.patch('/:videoId', requireUser, updateVideoHandler);
+
+router.get('/', findVideosHandler);
+
+router.get('/:videoId', streamVideoHandler);
 
 export default router;
